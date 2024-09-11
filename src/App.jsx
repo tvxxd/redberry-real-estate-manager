ef1a22e717ff47d6b91f39b87630bf58bb893a92
tvxxd <tavdgiridzenika1@gmail.com>
2024-09-11 22:03:57 +0400

    setup app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
-function App() {}
+import ListingPage from "./pages/ListingPage";
+import "./index.css";
+function App() {
+  return (
+    <div id="app">
+      <ListingPage />
+    </div>
+  );
+}
 
 export default App;
