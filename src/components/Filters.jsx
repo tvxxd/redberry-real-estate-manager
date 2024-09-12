d1c13a95518be9a37381f4ff42abf021f66384d1
tvxxd <tavdgiridzenika1@gmail.com>
2024-09-12 16:22:29 +0400

    adjust filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -3,6 +3,8 @@ import Shape from "../icons/Shape.jsx";
 import RegionModal from "./modals/RegionModal.jsx";
 import PriceModal from "./modals/PriceModal.jsx";
 import { useState } from "react";
+import AreaModal from "./modals/AreaModal.jsx";
+import RoomModal from "./modals/RoomModal.jsx";
 
 function Filters({
   regions,
@@ -15,41 +17,64 @@ function Filters({
   bedrooms,
   setBedrooms,
 }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  /**
+   * state for each filter modal
+   */
+  const [isModalOpen, setIsModalOpen] = useState({
+    region: false,
+    price: false,
+    area: false,
+    bedrooms: false,
+  });
+
+  /**
+   * selects modal for specific filter
+   * close all modals except the one that should be opened
+   * @param modalName name of modal
+   * @returns new state
+   */
+  const toggleModal = (modalName) => {
+    setIsModalOpen((prevState) => {
+      const newState = {
+        region: false,
+        price: false,
+        area: false,
+        bedrooms: false,
+      };
+      newState[modalName] = !prevState[modalName];
+      return newState;
+    });
+  };
 
   return (
     <div className={styles.filters}>
       <div className={styles.regions}>
         <span>რეგიონი</span>
-        <button
-          onClick={() =>
-            isModalOpen ? setIsModalOpen(false) : setIsModalOpen(true)
-          }
-          className={styles.btn}
-        >
+        <button onClick={() => toggleModal("region")} className={styles.btn}>
           <Shape />
         </button>
-        {isModalOpen && <RegionModal regions={regions} />}
+        {isModalOpen.region && <RegionModal regions={regions} />}
       </div>
       <div className={styles.price}>
         <span>საფასო კატეგორია</span>
-        <button
-          onClick={() =>
-            isModalOpen ? setIsModalOpen(false) : setIsModalOpen(true)
-          }
-          className={styles.btn}
-        >
+        <button onClick={() => toggleModal("price")} className={styles.btn}>
           <Shape />
         </button>
-        {isModalOpen && <PriceModal />}
+        {isModalOpen.price && <PriceModal />}
       </div>
       <div className={styles.area}>
         <span>ფართობი</span>
-        <Shape />
+        <button onClick={() => toggleModal("area")} className={styles.btn}>
+          <Shape />
+        </button>
+        {isModalOpen.area && <AreaModal />}
       </div>
       <div className={styles.rooms}>
         <span>საძინებლების რაოდენობა</span>
-        <Shape />
+        <button onClick={() => toggleModal("bedrooms")} className={styles.btn}>
+          <Shape />
+        </button>
+        {isModalOpen.bedrooms && <RoomModal />}
       </div>
     </div>
   );
