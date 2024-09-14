48f2436f282ed32eb7826fbac91c679ac05896bf
tvxxd <tavdgiridzenika1@gmail.com>
2024-09-14 14:27:16 +0400

    add priceformater2 and update JSdoc

--- src/utils/formatPrice.js
+++ src/utils/formatPrice.js
@@ -1,8 +1,17 @@
 /**
  * formats price
- * @param price coming from api
+ * @param price numbers
  * @returns formatted price ex: 50 000 | 500 000
  */
 export function formatPrice(price) {
   return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
+/**
+ * formats price
+ * @param price numbers
+ * @returns formatted price ex: 50, 000 | 500, 000
+ */
+export function formatPrice2(price) {
+  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ", ");
 }
