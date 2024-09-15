ff815560fef92d440a3351f18e6986f4253da7c1
tvxxd <tavdgiridzenika1@gmail.com>
2024-09-15 22:03:22 +0400

    include header only in app component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import { RegionsProvider } from "./context/RegionsContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddListingPage from "./pages/AddListingPage";
 import ListingDetailsPage from "./pages/ListingDetailsPage";
+import Header from "./components/Header";
 function App() {
   return (
     <div id="app">
       <ListingsProvider>
         <RegionsProvider>
           <Router>
+            <Header />
             <Routes>
               <Route path="/" element={<ListingPage />} />
               <Route path="/add-listing" element={<AddListingPage />} />
