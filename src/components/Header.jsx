28d0e3e619159b751c0adfbf433d0d730d30bff2
tvxxd <tavdgiridzenika1@gmail.com>
2024-09-15 22:37:44 +0400

    navigate to / on logo click

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import logo from "../assets/LOGO.png";
 import styles from "./Header.module.css";
 
 function Header() {
+  const navigate = useNavigate();
+  const handleSubmit = () => {
+    navigate("/");
+  };
   return (
     <div className={styles.header}>
-      <img src={logo} alt="REDBERRY LOGO" />
+      <img
+        onClick={handleSubmit}
+        style={{ cursor: "pointer" }}
+        src={logo}
+        alt="REDBERRY LOGO"
+      />
     </div>
   );
 }
