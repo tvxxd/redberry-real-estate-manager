ae9d4e3c45e94aefeac0fa219e052d1a5bff328d
tvxxd <tavdgiridzenika1@gmail.com>
2024-09-15 21:35:52 +0400

    route for add listing

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -4,8 +4,10 @@ import AddListingButton from "./AddListingButton";
 import styles from "./Controls.module.css";
 import AgentModal from "./modals/agent/AgentModal";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Controls() {
+  const navigate = useNavigate();
   const [isAddAgentModalOpen, setIsAddAgentModalOpen] = useState(false);
 
   // Toggler for addAgent modal
@@ -13,12 +15,16 @@ function Controls() {
     setIsAddAgentModalOpen(!isAddAgentModalOpen);
   };
 
+  const handleClick = () => {
+    navigate("/add-listing");
+  };
+
   return (
     <>
       <div className={styles.controls}>
         <Filters />
         <div className={styles.buttons}>
-          <AddListingButton />
+          <AddListingButton onClick={handleClick} />
           <AddAgentButton onClick={toggleModal} />
         </div>
         {isAddAgentModalOpen && <AgentModal onClose={toggleModal} />}
